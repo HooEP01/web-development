b79bf81d13c533b70027f5df081082fce65894c1
HOOEP01 <B200152B@sc.edu.my>
2023-02-23 11:11:11 +0800

    javascript dice & drum kit

---
+++ JavaScript/Dicee Challenge/index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+// var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+// var randomImageSource1 = "./images/" + randomDiceImage1;
+document.querySelector('.img1').setAttribute('src', './images/dice' + randomNumber1 + '.png')
+document.querySelector('.img2').setAttribute('src', './images/dice' + randomNumber2 + '.png')
+
+var text = "";
+if(randomNumber1 > randomNumber2) {
+    text = "Player 1 win!🚩"
+} 
+else if(randomNumber2 > randomNumber1) {
+    text = "Player 2 win!🚩"
+} 
+else {
+    text = "Draw!"
+}
+document.querySelector('h1').innerText = text;
