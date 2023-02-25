511b32be163b36c3ac854ea3fccccddf47ed7dac
HOOEP01 <B200152B@sc.edu.my>
2023-02-25 12:02:32 +0800

    node intro

---
+++ Node/index.js
@@ -0,0 +1,7 @@
+// const fs = require('node:fs');
+
+// fs.copyFileSync('file.txt', 'destination.txt');
+// console.log('file.txt was copied to destination.txt');
+
+const superheroes = require('superheroes');
+console.log(superheroes.random());
